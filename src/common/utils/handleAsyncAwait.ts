bc65919a01a0975a3ed29d7bd39413ba3eff76fe
Steve S. Samson <stevee.samson@gmail.com>
2020-04-08 09:14:34 +0100

    fix handleAsyncAwait error

--- src/common/utils/handleAsyncAwait.ts
+++ src/common/utils/handleAsyncAwait.ts
@@ -1,10 +1,6 @@
 import { HandleAsyncAwait } from "../types";
 
 const handle: HandleAsyncAwait = (promise: Promise<any>) =>
-	promise
-		.then(data => {
-			data;
-		})
-		.catch(error => Promise.resolve({ error }));
+	promise.then(data => ({ data })).catch(error => Promise.resolve({ error }));
 
 export default handle;
--- src/rest/restful.ts
+++ src/rest/restful.ts
@@ -9,23 +9,21 @@ const baseREST = (baseUrl: string, modelName: string): RouteConfig => {
 	map[`get ${baseUrl}`] = async (req: Request, res: Response) => {
 		const model = Models["get" + modelName](req);
 		const { error, data } = await raa(model.find(req.parameters));
-		if (data) {
-			res.status(200).json({ data });
-		} else {
+		if (error) {
 			console.error(error);
-			res.status(200).json({ error: error.sqlMessage });
+			return res.status(200).json({ error: error.sqlMessage });
 		}
+		res.status(200).json({ data });
 	};
 
 	map[`get ${baseUrl}/:id`] = async (req: Request, res: Response) => {
 		const model = Models["get" + modelName](req);
 		const { data, error } = await raa(model.find(req.parameters));
-		if (data) {
-			res.status(200).json({ data });
-		} else {
+		if (error) {
 			console.error(error.sqlMessage);
-			res.status(200).json({ error: error.sqlMessage });
+			return res.status(200).json({ error: error.sqlMessage });
 		}
+		res.status(200).json({ data });
 	};
 
 	map[`post ${baseUrl}`] = async (req: Request, res: Response) => {
@@ -34,14 +32,12 @@ const baseREST = (baseUrl: string, modelName: string): RouteConfig => {
 		const { error, data } = await raa(model.create(load));
 
 		// console.log("saved with: ", error, result);
-		if (data) {
-			const row = await model.find(data);
-			// console.log("New Row: ", row);
-			model.publishCreate(req, row);
-			res.status(200).json({ data: row });
-		} else {
-			res.status(200).json({ error: error.sqlMessage });
+		if (error) {
+			return res.status(200).json({ error: error.sqlMessage });
 		}
+		const row = await model.find(data);
+		model.publishCreate(req, row);
+		res.status(200).json({ data: row });
 	};
 
 	map[`put ${baseUrl}/:id`] = async (req: Request, res: Response) => {
