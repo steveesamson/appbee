2dc818a16ef57035619a48583677427c03d6d704
Steve S. Samson <stevee.samson@gmail.com>
2021-04-02 19:13:51 +0100

    extends jest timeout inside jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,7 @@
-import * as Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+// import * as Enzyme from "enzyme";
+// import Adapter from "enzyme-adapter-react-16";
 
-Enzyme.configure({
-	adapter: new Adapter(),
-});
+// Enzyme.configure({
+// 	adapter: new Adapter(),
+// });
+jest.setTimeout(40000);
