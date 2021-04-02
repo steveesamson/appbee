c04a0c182af8cf08fbbda94c0c31d6985a44b86b
Steve S. Samson <stevee.samson@gmail.com>
2021-04-02 18:26:45 +0100

    fix pull conflicts

--- src/common/utils/busMessenger.ts
+++ src/common/utils/busMessenger.ts
@@ -4,9 +4,6 @@ const redis: any = require("redis");
 
 class BusMessenger {
 	options: StoreConfig = { host: "127.0.0.1", port: 6379, retry_strategy: () => 1000 } as any;
-	constructor() {
-		console.log(`Event bus. Defaulting to host:${this.options.host}, port:${this.options.port}`);
-	}
 	configure(options: StoreConfig) {
 		this.options = options;
 		console.log(`Configuring event bus to use host:${this.options.host}, port:${this.options.port}`);
