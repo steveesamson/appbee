7b6c4feebd2072d004daca27d585f9d97fe166fe
Steve S. Samson <stevee.samson@gmail.com>
2021-04-03 01:08:42 +0100

    add log info to busmessenger

--- src/common/utils/busMessenger.ts
+++ src/common/utils/busMessenger.ts
@@ -4,6 +4,11 @@ const redis: any = require("redis");
 
 class BusMessenger {
 	options: StoreConfig = { host: "127.0.0.1", port: 6379, retry_strategy: () => 1000 } as any;
+	
+	constructor(){
+		console.log(`Using default config for event bus to use host:${this.options.host}, port:${this.options.port}`);
+	}
+	
 	configure(options: StoreConfig) {
 		this.options = options;
 		console.log(`Configuring event bus to use host:${this.options.host}, port:${this.options.port}`);
